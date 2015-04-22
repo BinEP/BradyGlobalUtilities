658ed709bc37f8b415f069b7d107a36b382fcd14
BinEP <bradystoffel@gmail.com>
2015-04-22 13:17:31 -0500

    Initial Commit

---
+++ src/sounds/BSSound.java
@@ -0,0 +1,18 @@
+package sounds;
+
+public abstract class BSSound implements BSSoundInterface {
+
+	public BSSound() {
+	}
+
+	@Override
+	public void playSound() {
+
+	}
+
+	@Override
+	public void setupSound(String fileName) {
+
+	}
+
+}
---
+++ src/sounds/BSSoundInterface.java
@@ -0,0 +1,6 @@
+package sounds;
+
+public interface BSSoundInterface {
+	public void playSound();
+	public void setupSound(String fileName);
+}
