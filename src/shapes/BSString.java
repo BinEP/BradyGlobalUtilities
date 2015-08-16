8d8fa5008a3705e2f2958fa928d651fa5c8d485b
Brady <Brady@todd-stoffels-imac.local>
2015-08-16 17:44:18 -0500

    String shape

---
+++ src/shapes/BSString.java
@@ -0,0 +1,49 @@
+package shapes;
+
+import java.awt.Font;
+import java.awt.Shape;
+import java.awt.font.GlyphVector;
+
+import javax.swing.JPanel;
+
+import game_actions.Control;
+import shapes.interfaces.BSShape;
+
+public class BSString extends BSShape {
+
+	public Font font;
+	public String text;
+	private JPanel panel = new JPanel();
+	
+	public BSString() {
+	}
+	
+	public BSString(String text, int x, int y) {
+		this.text = text;
+		this.x = x;
+		this.y = y;
+		font = Control.customFont.getCustomFont();
+	}
+	
+	public BSString(String text, Font font, int x, int y) {
+		this(text, x, y);
+		this.font = font;
+	}
+	
+	public BSString(String text, int x, int y, int dx, int dy) {
+		this (text, x, y);
+		deltaX = dx;
+		deltaY = dy;
+	}
+	
+	public BSString(String text, Font font, int x, int y, int dx, int dy) {
+		this(text, x, y, dx, dy);
+		this.font = font;
+	}
+	
+	@Override
+	public Shape getShape() {
+		GlyphVector v = font.createGlyphVector(panel.getFontMetrics(font).getFontRenderContext(), text);
+		return v.getOutline();
+	}
+}
