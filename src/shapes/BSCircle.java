e896ce54aceff0bf847afafd1b8ec01c294be356
Brady <Brady@todd-stoffels-imac.local>
2015-08-16 17:04:54 -0500

    Circle Added

---
+++ src/shapes/BSCircle.java
@@ -0,0 +1,31 @@
+package shapes;
+
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+
+import shapes.interfaces.BSShape;
+
+public class BSCircle extends BSShape {
+
+	public int radius = 0;
+	
+	public BSCircle() {
+		
+	}
+	public BSCircle(int x, int y, int r) {
+		this.x = x;
+		this.y = y;
+		this.radius = r;
+	}
+	
+	public BSCircle(int x, int y, int r, int dx, int dy) {
+		this (x, y, r);
+		deltaX = dx;
+		deltaY = dy;
+	}
+
+	@Override
+	public Shape getShape() {
+		return new Ellipse2D.Double(x, y, radius, radius);
+	}
+}
